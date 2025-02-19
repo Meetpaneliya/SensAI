0bc16eb71969e086fb7d20fba15b7c230086c3c5
Meetpaneliya <meetpaneliya032@gmail.com>
2025-02-19 12:56:50 +0530

    commit-5

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,13 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
   },
-  // Add this section
-  server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  }
 };
 
 export default nextConfig;
